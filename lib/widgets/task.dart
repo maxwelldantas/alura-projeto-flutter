70a8f55422073c6f7be86bc005284c6c32e40787
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-07-12 15:57:56 -0300

    Adicionadas bordas aos widgets

--- lib/widgets/task.dart
+++ lib/widgets/task.dart
@@ -20,20 +20,35 @@ class _TaskState extends State<Task> {
       padding: const EdgeInsets.all(8.0),
       child: Stack(
         children: [
-          Container(color: Colors.blue, height: 140),
+          Container(
+            height: 140,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Colors.blue,
+            ),
+          ),
           Column(
             children: [
               Container(
-                color: Colors.white,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                ),
                 height: 100,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
-                      color: Colors.black26,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.black26,
+                      ),
                       width: 72,
                       height: 100,
-                      child: Image.network(widget.imagem, fit: BoxFit.cover),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4),
+                        child: Image.network(widget.imagem, fit: BoxFit.cover),
+                      ),
                     ),
                     Stack(
                       children: [
