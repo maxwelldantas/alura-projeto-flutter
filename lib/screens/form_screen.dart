c7c967501f81195eb6e32e72f9e51925c961c426
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-07-14 21:11:33 -0300

    Adicionado obter imagem por URL e tratativa de erro

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -29,6 +29,8 @@ class _FormScreenState extends State<FormScreen> {
             border: Border.all(width: 3),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -63,6 +65,11 @@ class _FormScreenState extends State<FormScreen> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  onChanged: (text) {
+                    setState(() {
+                      imageController.text = text;
+                    });
+                  },
                   controller: imageController,
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
@@ -72,6 +79,30 @@ class _FormScreenState extends State<FormScreen> {
                     ),
                     fillColor: Colors.white,
                     filled: true,
+                  ),
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 72,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 2, color: Colors.blue),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    imageController.text,
+                    fit: BoxFit.cover,
+                    errorBuilder:
+                        (
+                          BuildContext context,
+                          Object exception,
+                          StackTrace? stackTrace,
+                        ) {
+                          return Image.asset('assets/images/nophoto.png');
+                        },
                   ),
                 ),
               ),
